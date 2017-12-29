f115fc318fa668e1b98d00c266c74d408ca540f7
DarkGizmo <francois.vaillancourt@hotmail.ca>
2017-12-28 23:45:55 -0500

    First pass at networked game w/ communication

---
+++ Source/CellDemo/CellDemo.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "CellDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CellDemo, "CellDemo" );
+
+DEFINE_LOG_CATEGORY(LogCellDemo)
+ 
